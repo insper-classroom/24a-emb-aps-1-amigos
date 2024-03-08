fcbb15d5470a2fa8ac1014f939b27a31a10a4fe9
Mariana <marianaaa1@al.insper.edu.br>
2024-03-08 15:32:43 +0000

    start e musica

--- main/main.c
+++ main/main.c
@@ -1,8 +1,8 @@
-/**
- * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
+
+//  * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+//  *
+//  * SPDX-License-Identifier: BSD-3-Clause
+//  */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,6 +21,7 @@ int BUTTON_PIN_RED =    12; //ok
 int BUTTON_PIN_GREEN =  27; //OK
 int BUTTON_PIN_BLUE =   5; //OK
 int BUTTON_PIN_YELLOW =18;  //ok
+int BUTTTON_PIN_START = 19;
 
 int buzzer = 3;
 
@@ -29,15 +30,16 @@ int volatile green = 0;
 int volatile red = 0;
 int volatile blue = 0;
 int volatile yellow = 0;
+int volatile start = 0;
 
 
-int pontuacao = 0;
-int sequencia[100];
-int rodada = 0;
-bool game_over = false;
+
 
 //******************* CALLBACKS ******************* 
 void btn_callback(uint gpio, uint32_t events) {
+    if (gpio == BUTTTON_PIN_START) {
+        start = 1;
+    }
 
     if (gpio == BUTTON_PIN_RED) {
         red = 1;
@@ -63,6 +65,45 @@ void barulho(int freq, int tempo, int pino){
         sleep_us(periodo/2);
     }
 }
+void tocar_musica_tema(int time_ms) {
+    uint64_t start_time = time_us_64();
+
+    //fur elise
+    // int notas[] = {659, 622, 659, 622, 659, 494, 587, 523, 587, 494, 587, 440, 523, 349, 415, 494, 659, 622, 659, 622, 659, 494, 587, 523, 587, 494, 587, 440, 523, 349, 415, 494, 659, 622, 659, 622, 659, 494, 587, 523, 587, 494, 587, 440, 523, 349, 415, 494, 659, 622, 659, 622, 659, 494, 587, 523, 587, 494, 587, 440, 523, 349, 415, 494};
+    // int duracoes[] = {200, 200, 200, 200, 200, 200, 200, 800, 200, 200, 200, 200, 600, 200, 200, 200, 600, 800, 200, 200, 200, 200, 200, 200, 200, 200, 800, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200};
+
+    // Definir as frequências das notas musicais correspondentes à melodia de Harry Potter (uma oitava acima)
+    int notas [] = {329, 440, 523, 494, 440, 329, 294, 494, 440, 523, 494, 392, 494, 329, 329, 440, 523, 494};
+    int duracoes[] = {300, 300, 300, 300, 300, 300, 600, 300, 300, 600, 300, 300, 600, 300, 300, 300, 300, 300};
+
+    // int notas[] = {329, 440, 523, 494, 440, 329, 294, 494, 440, 523, 494, 392, 494, 329, 329, 440, 523, 494, 440, 329, 392, 370, 349, 277, 349, 329, 329, 329, 523, 440, 523};
+    // int duracoes[] = {300, 300, 300, 300, 300, 300, 1000, 300, 300, 300, 300, 300, 300, 1000, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 1000, 300, 300, 1000};
+    // int duracoes[] = {300,300,300,300,300,300,600,300,300,300,300,300,600,300,300,300,300,300,300,300,300,300,300,300,300,300,600,300,300,600};
+    int num_notas = sizeof(notas) / sizeof(notas[0]);
+
+
+    for (int i = 0; i < num_notas; i++) {
+        if ((time_us_64() - start_time) / 1000 >= time_ms) {
+            break; } // Parar se já tiver passado o tempo especificado
+        for (int i = 0; i < num_notas ; i++) {
+            barulho(notas[i], duracoes[i], buzzer); 
+            sleep_ms(100);
+        }
+        // Mantém o buzzer ligado pelo tempo da nota
+         // Desliga o buzzer
+
+         // Breve pausa entre as notas
+         
+    }
+}
+
+
+void inicio() {
+    tocar_musica_tema(6000); // Toca a música tema de Harry Potter
+    
+}
+
+
 
 void buzzer_led(int led_pin) {
     int freq;
@@ -81,29 +122,22 @@ void buzzer_led(int led_pin) {
     gpio_put(led_pin, !gpio_get(led_pin));
 }
 
-void inicio() {
-    buzzer_led(LED_PIN_RED);
-    buzzer_led(LED_PIN_GREEN);
-    buzzer_led(LED_PIN_YELLOW);
-    buzzer_led(LED_PIN_BLUE);
-    sleep_ms(500);
-}
 
-void proximaRodada() {
+void proximaRodada(int rodada, int sequencia[]) {
     int cores[4] = {LED_PIN_BLUE, LED_PIN_GREEN, LED_PIN_RED, LED_PIN_YELLOW}; 
     int sorteio = rand() % 4;
     sequencia[rodada] = cores[sorteio];
     rodada++;
 }
 
-void reproduzirSequencia() {
+void reproduzirSequencia(int rodada, int sequencia[]) {
     for (int i=0; i < rodada; i++) {
         buzzer_led(sequencia[i]);
         sleep_ms(100);
     }
 }
 
-void aguardarJogador(){
+void aguardarJogador(int rodada, int sequencia[], bool game_over){
     for (int i=0; i<rodada; i++) {
         while ((red == 0) && (green == 0) && (blue ==0) && (yellow == 0)) {
             sleep_ms(10);
@@ -157,10 +191,15 @@ int main() {
     gpio_set_dir(LED_PIN_BLUE, GPIO_OUT);
     gpio_set_dir(LED_PIN_YELLOW, GPIO_OUT);
 
+    //botao start
+    gpio_set_dir(BUTTTON_PIN_START, GPIO_IN);
+    gpio_pull_up(BUTTTON_PIN_START);
+    gpio_set_irq_enabled_with_callback(BUTTTON_PIN_START, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &btn_callback);
+
     //botao red
     gpio_set_dir(BUTTON_PIN_RED, GPIO_IN);
     gpio_pull_up(BUTTON_PIN_RED);
-    gpio_set_irq_enabled_with_callback(BUTTON_PIN_RED, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &btn_callback);
+    gpio_set_irq_enabled(BUTTON_PIN_RED, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true);
     
     //botao green
     gpio_set_dir(BUTTON_PIN_GREEN, GPIO_IN);
@@ -180,23 +219,43 @@ int main() {
     gpio_init(buzzer);
     gpio_set_dir(buzzer, GPIO_OUT);
 
+    int sequencia[100];
+    int rodada = 0;
+    bool game_over = false;
+
     inicio();
 
-    while (true) {
-        // para iniciar um novo jogo
-        if (game_over){
-            rodada = 0;
-            red = 0;
-            green = 0;
-            blue = 0;
-            yellow = 0;
-            game_over = false;
-            sleep_ms(500);
-            inicio();
-        }
+    while (1) {
+        if (start) {
+            // para iniciar um novo jogo
+            if (game_over){
+                rodada = 0;
+                red = 0;
+                green = 0;
+                blue = 0;
+                yellow = 0;
+                game_over = false;
+                sleep_ms(500);
+                inicio();
+            }
 
-        proximaRodada();
-        reproduzirSequencia();
-        aguardarJogador();
-    }
+            proximaRodada(rodada, sequencia);
+            reproduzirSequencia(rodada, sequencia);
+            aguardarJogador(rodada, sequencia, game_over);
+        }
+        if (start == 0){
+            gpio_put(LED_PIN_RED, 0);
+            gpio_put(LED_PIN_GREEN, 0);
+            gpio_put(LED_PIN_BLUE, 0);
+            gpio_put(LED_PIN_YELLOW, 0);
+            // desliga o programa
+            
+        }
+            
+        }
+        
+        
 }
+
+
+
